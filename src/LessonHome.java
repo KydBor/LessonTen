1ac3ba56824719453b0e0f15ddcaca36ed3ede07
KydBor <e.susha@bk.ru>
2022-04-17 12:35:35 +0300

    Add Home

--- src/LessonHome.java
+++ src/LessonHome.java
@@ -1,5 +1,5 @@
 public class LessonHome {
-    public static void main(String[] args) {
+/*    public static void main(String[] args) {
         String a = "I like Java!!!";
 
         System.out.println("charAt - " + a.charAt(10));
@@ -11,5 +11,23 @@ public class LessonHome {
         System.out.println("toUpperCase - " + a.toUpperCase());
         System.out.println("toUpperCase - " + a.toLowerCase());
         System.out.println("substring - " + a.substring(6, 11));
+    }*/
+
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrom("Hello World abc toc sab cba aba bab rer olleH."));
+
+    }
+    private static boolean isPalindrom(String s){
+        String s1 = s.toLowerCase().replaceAll("(^A-Za-z)", "");
+        StringBuffer s2 = new StringBuffer(s1).reverse();
+        boolean s3 = true;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                s3 = false;
+                break;
+            }
+        }
+        return s3;
     }
 }
